e0b0188a406e426bf5026e966d7bb7fde461f68f
Rx10110 <orion4two@gmail.com>
2016-07-18 14:32:46 +0200

    Show and Index pages

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,11 @@
 class PicsController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
+    @pics = Pic.all.order('created_at DESC')
+  end
+
+  def show
   end
 
   def new
@@ -9,22 +14,41 @@ class PicsController < ApplicationController
 
   def create
     @pic = Pic.new(pic_params)
+    if @pic.save
+      redirect_to @pic, notice: "Pic posted!"
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    # if @pic.update(params[:pic].permit(:title, :description))
+    #   redirect_to @pic
+    # else
+    #   render 'edit'
+    # end
   end
 
+  def destroy
+    @pic.destroy
+    redirect_to root_path
+  end
+
+
   private
+
+  def find_pic
+    @pic = Pic.find(params[:id])
+  end
 
   def pic_params
     params.require(:pic).permit(
     :title,
     :description
     )
-
   end
 
 end
